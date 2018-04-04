a056e816c020a142f8a7c4b2fdf1ea6453381f24
James Campbell <james.campbell@tanti.org.uk>
2018-04-04 10:12:23 +0100

    Create stubs for testing websocket utils

--- tests/test_websocket_utils.py
+++ tests/test_websocket_utils.py
@@ -1,1 +1,45 @@
-from bitmex_manager import websocket_utils
+from bitmex_manager import websocket_utils as wu
+
+
+def test_get_ws_url():
+    assert 1 == 2
+
+
+def test_ws_connect():
+    assert 1 == 2
+
+
+def test_find_item_by_keys():
+    assert 1 == 2
+
+
+def test_on_message():
+    assert 1 == 2
+
+
+def test_on_open():
+    assert 1 == 2
+
+
+def test_on_close():
+    assert 1 == 2
+
+
+def test_on_error():
+    assert 1 == 2
+
+
+def test_get_auth():
+    assert 1 == 2
+
+
+def test_generate_nonce():
+    assert 1 == 2
+
+
+def test_generate_signature():
+    assert 1 == 2
+
+
+def test_disconnect():
+    assert 1 == 2
